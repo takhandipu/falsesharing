1a3a75f44bc647d21421acd08b466b3f7bf0714d
Tanvir Ahmed Khan <takh@bane.eecs.umich.edu>
2018-03-16 15:43:12 -0400

    NUMA machine check done for simple false sharing example on bane.eecs.umich.edu

--- mine/mine.sh
+++ mine/mine.sh
@@ -7,3 +7,6 @@ mv perf.data false.perf.data
 sudo ../../tmpGits/linux-4.15.10/tools/perf/perf c2c record -F 15000 -a --all-user ./no_false
 sudo ../../tmpGits/linux-4.15.10/tools/perf/perf c2c report -NN -c pid,iaddr --full-symbols --stdio > no_false.txt
 mv perf.data no_false.perf.data
+../../pmu-tools/ocperf.py stat -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm,mem_load_uops_l3_miss_retired.remote_hitm ./serial 2>> stat.txt
+../../pmu-tools/ocperf.py stat -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm,mem_load_uops_l3_miss_retired.remote_hitm ./false 2>> stat.txt
+../../pmu-tools/ocperf.py stat -r 100 -e mem_load_uops_l3_hit_retired.xsnp_hitm,mem_load_uops_l3_miss_retired.remote_hitm ./no_false 2>> stat.txt
